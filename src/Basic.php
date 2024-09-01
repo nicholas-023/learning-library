aa199d06d81c920951073a6fd09fb4515cce5da1
nicholas-023 <nicholastothework@gmail.com>
2024-09-01 11:09:18 +0700

    first commit

---
+++ src/Basic.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Basic {
+  public function __construct(string $name) {
+    $this->name = $name;
+  }
+
+  function increment(int $int) {
+    $int++;
+  }
+  
+  function helloWorld()
+  {
+    echo "Hello World";
+  }
+
+  function sayHello(string $name)
+  {
+    echo "Hello $name, My name is $this->name";
+  }
+}
+
+?>
